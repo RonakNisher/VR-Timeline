8e086f0fb5a3828f5021710a5c05b609b6fe3e69
juhidesai <juhi.nmims@gmail.com>
2016-04-22 16:36:20 -0400

    integrated code

--- Demo/Assets/head_scroll.cs
+++ Demo/Assets/head_scroll.cs
@@ -4,9 +4,13 @@ using System.Collections;
 
 public class head_scroll : MonoBehaviour {
 
-    public float speed;
+    public float speed = 1.5f;
 
-    public float threshold;
+    public float threshold1 = 0.5f;
+
+	public float threshold2 = 0.8f;
+
+	public float threshold3 = 0.9f;
 
     public CardboardHead head;
 
@@ -14,16 +18,41 @@ public class head_scroll : MonoBehaviour {
     void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+
+		if (transform.position.x > -5250 && transform.position.x < 5250) {
 //        transform.position += speed * head.Gaze.direction;
-        if(Math.Abs(head.Gaze.direction.x) > threshold)
-        {
-            Vector3 position = transform.position;
-            position.x -= speed * head.Gaze.direction.x;
-            transform.position = position;
-        }
-        
+			if (Math.Abs (head.Gaze.direction.x) > threshold3) {
+				Vector3 position = transform.position;
+				position.x -= 3 * speed * head.Gaze.direction.x;
+				transform.position = position;
+			}
+			if (Math.Abs (head.Gaze.direction.x) > threshold2) {
+				Vector3 position = transform.position;
+				position.x -= 2 * speed * head.Gaze.direction.x;
+				transform.position = position;
+			}
+			if (Math.Abs (head.Gaze.direction.x) > threshold1) {
+				Vector3 position = transform.position;
+				position.x -= speed * head.Gaze.direction.x;
+				transform.position = position;
+			}
+
+		} else if (transform.position.x <= -5250)
+		{
+			//Debug.Log (transform.position.x);
+			Vector3 position = transform.position;
+			position.x = -5200;
+			transform.position = position;
+		}
+		else if (transform.position.x >= 5250)
+		{
+			//Debug.Log (transform.position.x);
+			Vector3 position = transform.position;
+			position.x = 5200;
+			transform.position = position;
+		}
     }
 }
---
+++ Demo/Assets/load_scene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class load_scene : MonoBehaviour {
+
+	public void LoadScene(string name)
+    {
+        Debug.Log(name);
+        SceneManager.LoadScene(name);
+    }
+}
