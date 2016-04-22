7f4373cf941bf50eb742000bb6a5fa626085d3f0
juhidesai <juhi.nmims@gmail.com>
2016-04-22 11:19:22 -0400

    ease in easi out code

--- Demo/Assets/head_scroll.cs
+++ Demo/Assets/head_scroll.cs
@@ -4,9 +4,13 @@ using System.Collections;
 
 public class head_scroll : MonoBehaviour {
 
-    public float speed;
+    public float speed = 1.5f;
 
-    public float threshold;
+    public float threshold1 = 0.5f;
+
+	public float threshold2 = 0.8f;
+
+	public float threshold3 = 0.9f;
 
     public CardboardHead head;
 
@@ -18,12 +22,25 @@ public class head_scroll : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 //        transform.position += speed * head.Gaze.direction;
-        if(Math.Abs(head.Gaze.direction.x) > threshold)
+        if(Math.Abs(head.Gaze.direction.x) > threshold3)
         {
             Vector3 position = transform.position;
-            position.x -= speed * head.Gaze.direction.x;
+            position.x -= 3*speed * head.Gaze.direction.x;
             transform.position = position;
         }
+		if(Math.Abs(head.Gaze.direction.x) > threshold2)
+		{
+			Vector3 position = transform.position;
+			position.x -= 2*speed * head.Gaze.direction.x;
+			transform.position = position;
+		}
+		if(Math.Abs(head.Gaze.direction.x) > threshold1)
+		{
+			Vector3 position = transform.position;
+			position.x -= speed * head.Gaze.direction.x;
+			transform.position = position;
+		}
+
         
     }
 }
